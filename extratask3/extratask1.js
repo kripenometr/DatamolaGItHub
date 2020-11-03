ab31afbe340e39504ff07cf5bc9c710f57a57d61
MihailDatamola <mihail_polivoda@mail.ru>
2020-11-03 22:03:02 +0300

    ExtraTask3

--- extratask3/extratask1.js
+++ extratask3/extratask1.js
@@ -10,7 +10,7 @@ function max_sum() {
     let res = 0;
 
     for (let i = 0; i < n; ++i)
-		for (let j = i; j < n; ++j){
+        for (let j = i; j < n; ++j) {
             for (let k = i; k <= j; ++k)
                 sum += Number(input_arr[k]);
 
